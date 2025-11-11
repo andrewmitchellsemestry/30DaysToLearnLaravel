7c3994ffc96e9a4c48eadd576287597fc5c3de72
Andrew M <andrew.mitchell@semestry.com>
2025-11-11 16:42:57 +0000

    Fixed use statements in web route file

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Models\Job;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     Log::info('Welcome page visited');
