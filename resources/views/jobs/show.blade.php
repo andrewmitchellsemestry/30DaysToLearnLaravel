def85d00659f9292249b650d1ae24ca74ac932c5
Andrew M <andrew.mitchell@semestry.com>
2025-11-12 14:27:53 +0000

    Finish day 18. Looking at how we can perform CRUD actions on our jobs

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,8 +2,12 @@
     <x-slot:heading>
         Job
     </x-slot:heading>
-    <h2 class="font-bold text-lg">{{ $job['position'] }}</h2>
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
     <p>
         This job will pay: ${{ number_format($job['salary']) }}
+    </p>
+
+    <p class='mt-6'>
+        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
     </p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('home');
 });
 
+
+// Show all
 Route::get('/jobs', function () {
     Log::info('Jobs page visited');
     // This is an example of eager loading. We are getting all the necessary data that we want to manipulate. Don't just use the old method of just passing through jobs as data variable of the return view section
@@ -44,21 +46,25 @@ Route::get('/jobs', function () {
      */
 });
 
+// Show create
 Route::get('/jobs/create', function () {
     Log::info('Job create page visited');
     return view('jobs.create');
 });
 
+// Show one job
 Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
     Log::info('Job specific page visited');
+    $job = Job::find($id);
     return view('jobs.show', ['job' => $job]);
 });
 
+// Create
 Route::post('/jobs', function() {
+    Log::info('Job create request');
     request()->validate([
         'title' => ['required', 'min:3'],
-        'salary' => ['required'] 
+        'salary' => ['required', 'numeric:strict'] 
     ]);
 
     // The request() object lets us access data from the form data
@@ -68,6 +74,38 @@ Route::post('/jobs', function() {
         'employer_id' => 1
     ]);
 
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    Log::info('Job edit page visited');
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function($id) {
+    Log::info('Job update request');
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required', 'numeric:strict'] 
+    ]);
+    
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+// Delete
+Route::delete('/jobs/{id}', function ($id) {
+    Log::info('Job delete request');
+    $job = Job::findOrFail($id)->delete();
     return redirect('/jobs');
 });
 
